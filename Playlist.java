a47eb5e802c56e186dd1416453ff6998e8dfc042
jcabujat <50434298+jcabujat@users.noreply.github.com>
2019-10-23 11:39:12 +0800

    Update Playlist.java

--- Playlist.java
+++ Playlist.java
@@ -1,54 +1,48 @@
 package com.JonathanCabujat;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Playlist {
     private String playList;
-    private ArrayList<Album> albums;
+    private LinkedList<Song> songs;
 
     public Playlist(String name) {
         this.playList = name;
-        this.albums = new ArrayList<>();
+        this.songs = new LinkedList<>();
     }
 
-    public void add(Album album){
-        if (albums.isEmpty()) {
-            albums.add(album);
-            System.out.println(album.getAlbum() + " successfully added to " + playList);
-            return;
-        }
-        if (!albumStored(album.getAlbum())) {
-            albums.add(album);
-            System.out.println(album.getAlbum() + " successfully added to " + playList);
-        } else {
-            System.out.println(album.getAlbum() + " already existing on " + playList);
-        }
+    public String getPlayList() {
+        return playList;
     }
 
     public LinkedList<Song> getSongs() {
-        Iterator<Album> albumList = this.albums.iterator();
-        LinkedList<Song> songList = new LinkedList<>();
-        while (albumList.hasNext()) {
-            Album albumStored = albumList.next();
-            songList.addAll(albumStored.getSongs());
-        }
-        return songList;
+        return this.songs;
     }
 
-    private boolean albumStored(String albumName) {
-        Iterator<Album> albumList = this.albums.iterator();
+    public void add(Album album) {
+        LinkedList<Song> albumSongs = album.getSongs();
+        for (Song songFromAlbum : albumSongs) {
+            if (!songOnList(songFromAlbum)) {
+                this.songs.add(songFromAlbum);
+                System.out.println(songFromAlbum.toString() + " added to " + this.playList);
+            } else {
+                System.out.println(songFromAlbum.toString() + " already on " + this.playList);
+            }
+        }
+    }
+
+    private boolean songOnList(Song song) {
+        Iterator<Song> songList = songs.iterator();
         boolean alreadyStored = false;
-        while (albumList.hasNext()) {
-            Album albumStored = albumList.next();
-            if (albumStored.getAlbum().equals(albumName)) {
+        while (songList.hasNext()) {
+            Song songNext = songList.next();
+            if (songNext.getTitle().equals(song.getTitle())) {
                 alreadyStored = true;
                 break;
             }
         }
         return alreadyStored;
     }
-
 
 }
