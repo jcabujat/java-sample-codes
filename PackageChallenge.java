2b83059c2db0a458482dd264b7b8e4b61ca8242a
jcabujat <50434298+jcabujat@users.noreply.github.com>
2019-11-03 17:42:47 +0800

    Add files via upload

---
+++ PackageChallenge.java
@@ -0,0 +1,13 @@
+package com.packagesample;
+
+import com.JonathanCabujat.Series;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(Series.nSum(2));
+        System.out.println(Series.factorial(2));
+        Series.fibonacci(2);
+    }
+}
